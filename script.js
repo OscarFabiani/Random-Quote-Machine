9fc6ada43d1f208571bba927774b6e40812a18fc
OscarFabiani <spartan539@yahoo.com>
2019-02-17 17:04:32 -0800

    fixed repeats of random colors/quotes and disabled animation reset while animating

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const quotes = [
   ['-The Godfather', '"I\'m going to make him an offer he can\'t refuse.'],
   ['-The Wizard of Oz', '"Toto, I\'ve got a feeling we\'re not in Kansas anymore.'],
   ['-Jerry Macquire', '"Show me the money!'],
-  ['-A Few Good Men', '"You can\t handle the truth!'],
+  ['-A Few Good Men', '"You can\'t handle the truth!'],
   ['-Forrest Gump', '"Mama always said life was like a box of chocolates. You never know what you\'re gonna get.'],
   ['-The Shawshank Redemption', '"Get busy livin\', or get busy dyin\''],
   ['-Rocky', '"He doesn\'t know it\'s a show, he thinks it\'s a fight!'],
@@ -25,25 +25,46 @@ function random(list) {
   return Math.floor(Math.random() * list.length);
 }
 function randomQuote() {
-  text.innerHTML = '';
-  author.innerHTML = '';
+  //TRY TO MIGRATE TEXT/AUTHOR COLOR CHANGE AND INCORPORATE TEXT/AUTHOR FADE OUT/IN
   let index = random(quotes);
-  text.appendChild(document.createTextNode(quotes[index][1]));
-  author.appendChild(document.createTextNode(quotes[index][0]));
+  //checking if current quote/author is the same as the replacement and re-randomizes replacement if so.
+  while (author.innerText == quotes[index][0]) {
+    index = random(quotes);
+  }
+  //sets repacement quote/author.
+  text.innerHTML = quotes[index][1];
+  author.innerHTML = quotes[index][0];
 }
 function colorChange() {
   //Remove class(if present), trigger reflow, and add class.
+  let color = colors[random(colors)];
+  while (color == body.style.backgroundColor) {
+    color = colors[random(colors)];
+  }
+  body.style.setProperty('--color1', color);
+  text.style.setProperty('--color1', color);
+  author.style.setProperty('--color1', color);  
+  newQuote.style.setProperty('--color1', color);
+  tweetQuote.style.setProperty('--color1', color);
   body.classList.remove('anim1');
   text.classList.remove('anim2');
+  //text.classList.remove('anim3'); //trying to incorporate text fade out/in
   author.classList.remove('anim2');
   newQuote.classList.remove('anim1');
   tweetQuote.classList.remove('anim1');
   body.offsetHeight; //trigger reflow
   body.classList.add('anim1');
   text.classList.add('anim2');
+  //text.classList.add('anim3'); //trying to incorporate text fade out/in
   author.classList.add('anim2');
   newQuote.classList.add('anim1');
   tweetQuote.classList.add('anim1');
+  body.addEventListener('animationend', function() {body.style.backgroundColor = color;});
+  text.addEventListener('animationend', function() {text.style.color = color;});
+  author.addEventListener('animationend', function() {author.style.color = color;});
+  newQuote.addEventListener('animationend', function() {newQuote.style.backgroundColor = color;});
+  tweetQuote.addEventListener('animationend', function() {tweetQuote.style.backgroundColor = color;});
+
   /*
   //Adding a class and adding an event listener for animationend to remove the class.
   body.classList.add('anim1');
@@ -66,8 +87,19 @@ function colorChange() {
   */
 }
 
-newQuote.addEventListener('click', randomQuote, false);
-newQuote.addEventListener('click', colorChange, false);
+//disabling event while callback function(animation) is running
+let animating = false;
+function inhibitor() {
+  if (animating == false) {
+    animating = true;
+    colorChange();
+    randomQuote();
+  }
+}
+body.addEventListener('animationend', function() {animating = false});
+
+newQuote.addEventListener('click', inhibitor);
+newQuote.addEventListener('click', inhibitor);
 window.addEventListener('load', randomQuote);
 window.addEventListener('load', colorChange);
 
